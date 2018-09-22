0d265aec9068401e52edcac315275ff1ef8401d4
Phat Chiem <thachphat0@gmail.com>
2018-09-22 10:45:25 +0700

    Initial Commit

---
+++ TSLoan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TSLoan
+//
+//  Created by Phat Chiem on 9/22/18.
+//  Copyright © 2018 Phat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
